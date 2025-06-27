b6dbf013136d0bd44cdc21c808728f0720934bce
Anurag-singh-RBU <anuragsingh260805@gmail.com>
2025-06-28 01:57:45 +0530

    first commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
 import AddToDo from "@/components/AddToDo";
+import { Suspense } from "react";
 import Navbar from "@/components/Navbar";
 import Todos from "@/components/Todos";
 import {TodosProvider} from "@/store/todos"; 
@@ -12,9 +13,13 @@ export default function Home() {
         TODO APP
       </div>
       <main className="flex flex-col space-y-5 items-center min-h-screen">
+        <Suspense fallback={<div>Loading !!</div>}>
         <Navbar/>
+        </Suspense>
         <AddToDo/>
+        <Suspense fallback={<div>Loading !!</div>}>
         <Todos/>
+        </Suspense>
       </main>
     </TodosProvider>
 
